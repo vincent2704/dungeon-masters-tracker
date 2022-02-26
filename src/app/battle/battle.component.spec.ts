cafa2e9fd3a11068035aab944e34218081f40226
vincent2704 <biernacki.jj@gmail.com>
2022-02-26 12:46:17 +0100

    initiative conflict resolving - initiative to actors map

--- src/app/battle/battle.component.spec.ts
+++ src/app/battle/battle.component.spec.ts
@@ -4,6 +4,7 @@ import {BattleComponent} from './battle.component';
 import {PrepareBattleComponent} from "./prepare-battle/prepare-battle.component";
 import {FormsModule} from "@angular/forms";
 import {AddActorComponent} from "./add-actor/add-actor.component";
+import {Actor} from "../models/actor";
 
 describe('BattleComponent', () => {
   let battleComponent: BattleComponent;
@@ -26,4 +27,25 @@ describe('BattleComponent', () => {
   it('should create', () => {
     expect(battleComponent).toBeTruthy();
   });
+
+  it("should successfully build initiative to actor's map", () => {
+    //given
+    let actor1 = new Actor('Actor 1', 10, 10, 1);
+    let actor2 = new Actor('Actor 2', 10, 10, 1);
+    let actor3 = new Actor('Actor 3', 10, 10, 2);
+    let actor4 = new Actor('Actor 4', 10, 10, 2);
+    let actor5 = new Actor('Actor 5', 10, 10, 3);
+
+    let expectedMap = (new Map<number, Actor[]>());
+    expectedMap.set(1, [actor1, actor2]);
+    expectedMap.set(2, [actor3, actor4]);
+    expectedMap.set(3, [actor5]);
+
+    battleComponent.actors = [actor1, actor2, actor3, actor4, actor5];
+    //when
+    let map = battleComponent.getInitiativeConflictedActors();
+    //then
+    expect(map).toEqual(expectedMap);
+  });
+
 });
