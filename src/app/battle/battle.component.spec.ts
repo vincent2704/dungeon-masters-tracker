f0421776046b62b74e2975880028e831192544a3
vincent2704 <biernacki.jj@gmail.com>
2022-03-06 13:37:50 +0100

    initiative solving implemented, works for one conflict at this moment

--- src/app/battle/battle.component.spec.ts
+++ src/app/battle/battle.component.spec.ts
@@ -28,49 +28,4 @@ describe('BattleComponent', () => {
     expect(battleComponent).toBeTruthy();
   });
 
-  it("should successfully build initiative to actors map", () => {
-    //given
-    let actor1 = new Actor('Actor 1', 10, 10, 1);
-    let actor2 = new Actor('Actor 2', 10, 10, 1);
-    let actor3 = new Actor('Actor 3', 10, 10, 2);
-    let actor4 = new Actor('Actor 4', 10, 10, 2);
-    let actor5 = new Actor('Actor 5', 10, 10, 3);
-
-    let expectedMap = (new Map<number, Actor[]>());
-    expectedMap.set(1, [actor1, actor2]);
-    expectedMap.set(2, [actor3, actor4]);
-    expectedMap.set(3, [actor5]);
-
-    battleComponent.actors = [actor1, actor2, actor3, actor4, actor5];
-    //when
-    let map = battleComponent.getInitiativeConflictedActors();
-    //then
-    expect(map).toEqual(expectedMap);
-  });
-
-  it("should properly sort actors by their priority order", () => {
-    //given
-    let actor1 = new Actor('Actor 1', 10, 1, 1);
-    let actor2 = new Actor('Actor 2', 10, 1, 2);
-    let actor3 = new Actor('Actor 3', 10, 1, 2);
-    let actor4 = new Actor('Actor 4', 10, 1, 2);
-    let actor5 = new Actor('Actor 5', 10, 1, 3);
-
-    battleComponent.actors = [actor5, actor2, actor3, actor4, actor1]; //with the initiative and alphabetical order
-    fixture.detectChanges();
-
-    let conflictedActorsToPriorityMap = new Map<Actor, number>();
-    conflictedActorsToPriorityMap.set(actor2, 3);
-    conflictedActorsToPriorityMap.set(actor3, 1);
-    conflictedActorsToPriorityMap.set(actor4, 2);
-    battleComponent.conflictedActorsToPriorityOrderNumbersMap = new Map<Actor, number>();
-
-    //when
-    battleComponent.sortActorsByPriority();
-
-    //then
-    expect(battleComponent.actors).toEqual(
-      [actor5, actor3, actor4, actor2, actor1]);
-  });
-
 });
