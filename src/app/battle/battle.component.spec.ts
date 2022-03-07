3bab798abf8766b4f85e4958625e1a4a40ecde1c
vincent2704 <biernacki.jj@gmail.com>
2022-03-07 02:05:45 +0100

    bugfix - leave only protagonists with their current HP after battle end, test TBD

--- src/app/battle/battle.component.spec.ts
+++ src/app/battle/battle.component.spec.ts
@@ -4,7 +4,6 @@ import {BattleComponent} from './battle.component';
 import {PrepareBattleComponent} from "./prepare-battle/prepare-battle.component";
 import {FormsModule} from "@angular/forms";
 import {AddActorComponent} from "./add-actor/add-actor.component";
-import {Actor} from "../models/actor";
 
 describe('BattleComponent', () => {
   let battleComponent: BattleComponent;
