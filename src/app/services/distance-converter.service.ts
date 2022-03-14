fd09208806d5bfb1c0aaba0aee13f987a83695e0
vincent2704 <biernacki.jj@gmail.com>
2022-03-14 15:33:31 +0100

    added imperial to SI distance converter service

---
+++ src/app/services/distance-converter.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DistanceConverterService {
+  private readonly feetToMetersRatio: number = 0.3;
+  private readonly milesToKilometersRatio: number = 1.5;
+
+  constructor() { }
+
+  convertFeetToMeters(feet: number): number {
+    return feet * this.feetToMetersRatio;
+  }
+
+  convertMilesToKilometers(miles: number): number {
+    return miles * this.milesToKilometersRatio;
+  }
+}
