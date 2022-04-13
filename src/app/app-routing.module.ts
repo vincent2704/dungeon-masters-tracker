16d13055415c43b2057ac487489927714a4ce875
vincent2704 <biernacki.jj@gmail.com>
2022-04-14 00:24:08 +0200

    added routing to app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BattleComponent} from "./battle/battle.component";
+import {ToolsComponent} from "./tools/tools.component";
+import {CheatSheetsComponent} from "./cheat-sheets/cheat-sheets.component";
+import {SettingsComponent} from "./settings/settings.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'battle', component: BattleComponent },
+  { path: 'cheat-sheets', component: CheatSheetsComponent },
+  { path: 'tools', component: ToolsComponent },
+  { path: 'settings', component: SettingsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,36 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  showBattleComponent: boolean = false;
-  showCheatSheetComponent: boolean = false;
-  showToolsComponent: boolean = false;
-  showSettingsComponent: boolean = false;
-
-  onShowBattleComponent() {
-    this.showBattleComponent = true;
-    this.showCheatSheetComponent = false;
-    this.showToolsComponent = false;
-    this.showSettingsComponent = false;
-  }
-
-  onShowCheatSheetComponent() {
-    this.showBattleComponent = false;
-    this.showCheatSheetComponent = true;
-    this.showToolsComponent = false;
-    this.showSettingsComponent = false;
-  }
-
-  onShowToolsComponent() {
-    this.showBattleComponent = false;
-    this.showCheatSheetComponent = false;
-    this.showToolsComponent = true;
-    this.showSettingsComponent = false;
-  }
-
-  onShowSettingsComponent() {
-    this.showBattleComponent = false;
-    this.showCheatSheetComponent = false;
-    this.showToolsComponent = false;
-    this.showSettingsComponent = true;
-  }
 }
