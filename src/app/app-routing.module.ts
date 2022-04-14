a992cd5b58fd2f51105151cfe2268b49e4f9e8dc
vincent2704 <biernacki.jj@gmail.com>
2022-04-14 17:29:52 +0200

    added routes for cheat sheets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,32 @@ import {BattleComponent} from "./battle/battle.component";
 import {ToolsComponent} from "./tools/tools.component";
 import {CheatSheetsComponent} from "./cheat-sheets/cheat-sheets.component";
 import {SettingsComponent} from "./settings/settings.component";
+import {
+  AbilityChecksCheatSheetComponent
+} from "./cheat-sheets/ability-checks-cheat-sheet/ability-checks-cheat-sheet.component";
+import {CombatEncounterComponent} from "./cheat-sheets/combat-encounter/combat-encounter.component";
+import {CoverCheatSheetComponent} from "./cheat-sheets/cover-cheat-sheet/cover-cheat-sheet.component";
+import {TravellingCheatSheetComponent} from "./cheat-sheets/travelling-cheat-sheet/travelling-cheat-sheet.component";
 
 const routes: Routes = [
   { path: 'battle', component: BattleComponent },
-  { path: 'cheat-sheets', component: CheatSheetsComponent },
+  {
+    path: 'cheat-sheets', component: CheatSheetsComponent,
+    children: [
+      { path: 'ability-checks', component: AbilityChecksCheatSheetComponent },
+      { path: 'combat-encounter', component: CombatEncounterComponent },
+      { path: 'cover', component: CoverCheatSheetComponent },
+      { path: 'traveling', component: TravellingCheatSheetComponent },
+    ]
+  },
   { path: 'tools', component: ToolsComponent },
   { path: 'settings', component: SettingsComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/cheat-sheets/cheat-sheets.component.ts
+++ src/app/cheat-sheets/cheat-sheets.component.ts
@@ -6,41 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cheat-sheets.component.css']
 })
 export class CheatSheetsComponent implements OnInit {
-  showTravellingCheatSheet: boolean = false;
-  showCoverCheatSheet: boolean = false;
-  showAbilityChecksCheatSheet: boolean = false;
-  showCombatEncounterCheatSheet: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onShowTravellingCheatSheet() {
-    this.showTravellingCheatSheet = !this.showTravellingCheatSheet;
-    this.showCoverCheatSheet = false;
-    this.showAbilityChecksCheatSheet = false;
-    this.showCombatEncounterCheatSheet = false;
-  }
-
-  onShowCoverCheatSheet() {
-    this.showTravellingCheatSheet = false;
-    this.showCoverCheatSheet = !this.showCoverCheatSheet;
-    this.showAbilityChecksCheatSheet = false;
-    this.showCombatEncounterCheatSheet = false;
-  }
-
-  onShowAbilityChecksCheatSheet() {
-    this.showTravellingCheatSheet = false;
-    this.showCoverCheatSheet = false;
-    this.showAbilityChecksCheatSheet = !this.showAbilityChecksCheatSheet;
-    this.showCombatEncounterCheatSheet = false;
-  }
-
-  onShowCombatEncounterCheatSheet() {
-    this.showTravellingCheatSheet = false;
-    this.showCoverCheatSheet = false;
-    this.showAbilityChecksCheatSheet = false;
-    this.showCombatEncounterCheatSheet = !this.showCombatEncounterCheatSheet;
-  }
 }
