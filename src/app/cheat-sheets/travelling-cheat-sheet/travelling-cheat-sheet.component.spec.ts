116dbf8cb6e2112dc395dd9cd837ebea8d93ebdd
vincent2704 <biernacki.jj@gmail.com>
2022-04-21 18:54:38 +0200

    adding/removing actors from campaign

--- src/app/cheat-sheets/travelling-cheat-sheet/travelling-cheat-sheet.component.spec.ts
+++ src/app/cheat-sheets/travelling-cheat-sheet/travelling-cheat-sheet.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TravellingCheatSheetComponent } from './travelling-cheat-sheet.component';
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('TravellingCheatSheetComponent', () => {
   let component: TravellingCheatSheetComponent;
@@ -8,6 +9,9 @@ describe('TravellingCheatSheetComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [ TravellingCheatSheetComponent ]
     })
     .compileComponents();
--- src/app/services/actor.service.spec.ts
+++ src/app/services/actor.service.spec.ts
@@ -75,4 +75,33 @@ describe('actorService', () => {
     expect(actor3.isActorTurnProgressed()).toEqual(false);
   });
 
+  it("should remove actor", () => {
+    // given
+    let actor1 = new Actor('Actor 1', 1);
+    let actor2 = new Actor('Actor 2', 1);
+    let actor3 = new Actor('Actor 3', 1);
+    actorService.setActors([actor1, actor2, actor3]);
+
+    // when
+    actorService.deleteActor(actor2);
+    let expectedActors = [actor1, actor3];
+
+    expect(actorService.getActors()).toEqual(expectedActors);
+  });
+
+  it("should remove multiple actors", () => {
+    // given
+    let actor1 = new Actor('Actor 1', 1);
+    let actor2 = new Actor('Actor 2', 1);
+    let actor3 = new Actor('Actor 3', 1);
+    actorService.setActors([actor1, actor2, actor3]);
+
+    // when
+    let notPresentActor = new Actor('Not present actor', 1);
+    let actorsToDelete = [actor1, actor3, notPresentActor];
+    actorService.deleteActors(actorsToDelete);
+
+    expect(actorService.getActors()).toEqual([actor2]);
+  });
+
 });
