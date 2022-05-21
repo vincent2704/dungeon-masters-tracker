01698b251c5e74cf7c77a735c2b5c2d56787e2fc
vincent2704 <biernacki.jj@gmail.com>
2022-05-21 11:49:54 +0200

    setting and getting current date and time

--- src/app/campaign-overview/time-configuration/time-configuration.component.ts
+++ src/app/campaign-overview/time-configuration/time-configuration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {NgbCalendar, NgbDateStruct} from "@ng-bootstrap/ng-bootstrap";
+import {NgbCalendar, NgbDateStruct, NgbTimeStruct} from "@ng-bootstrap/ng-bootstrap";
+import {TemporalService} from "../../services/temporal/temporal.service";
 
 @Component({
   selector: 'app-time-configuration',
@@ -8,18 +9,28 @@ import {NgbCalendar, NgbDateStruct} from "@ng-bootstrap/ng-bootstrap";
 })
 export class TimeConfigurationComponent implements OnInit {
 
-  model: NgbDateStruct;
-  date?: {year: number, month: number};
-  pickerTime = {hour: 13, minute: 30};
+  dateModel: NgbDateStruct;
+  timeModel: NgbTimeStruct;
+  currentDate: Date;
 
   isCollapsed: boolean = true;
 
-  constructor(private calendar: NgbCalendar) {
-    this.model = this.calendar.getToday();
-
+  constructor(private calendar: NgbCalendar, private temporalService: TemporalService) {
+    this.currentDate = temporalService.getCurrentDate();
+    this.dateModel = { year: this.currentDate.getFullYear(), month: this.currentDate.getMonth()+1, day: this.currentDate.getDate() };
+    this.timeModel = { hour: this.currentDate.getHours(), minute: this.currentDate.getMinutes(), second: this.currentDate.getSeconds() };
   }
 
   ngOnInit(): void {
+  }
+
+  onConfirmDate() {
+    this.temporalService.setCurrentDate(this.dateModel, this.timeModel);
+    this.currentDate = this.temporalService.getCurrentDate();
+  }
+
+  getCurrentDateModel(): NgbDateStruct {
+    return this.dateModel;
   }
 
 }
--- src/app/services/temporal/temporal.service.ts
+++ src/app/services/temporal/temporal.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {NgbDateStruct, NgbTimeStruct} from "@ng-bootstrap/ng-bootstrap";
 
 /*
   Service that manages time progress in the campaign
@@ -8,5 +9,25 @@ import { Injectable } from '@angular/core';
 })
 export class TemporalService {
 
-  constructor() { }
+  private currentDate: Date;
+
+  constructor() {
+    //TODO: backend call
+    this.currentDate = new Date(
+      1524, 6, 17,
+      18, 30, 0
+    );
+  }
+
+  setCurrentDate(newDate: NgbDateStruct, newTime: NgbTimeStruct) {
+    this.currentDate = new Date(
+      // month-1 because NgbDateStruct counts months from 1 while Date counts months from 0
+      newDate.year, newDate.month-1, newDate.day,
+      newTime.hour, newTime.minute, newTime.second
+    );
+  }
+
+  getCurrentDate(): Date {
+    return this.currentDate;
+  }
 }
