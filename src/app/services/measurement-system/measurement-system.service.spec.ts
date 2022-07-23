14a88793fb8a6aee8590042a14c34a4bedd34f97
vincent2704 <biernacki.jj@gmail.com>
2022-07-23 15:49:32 +0200

    styling - gradient to Monster cards

--- src/app/services/measurement-system/measurement-system.service.spec.ts
+++ src/app/services/measurement-system/measurement-system.service.spec.ts
@@ -16,16 +16,34 @@ describe('MeasurementSystemService', () => {
 
   it('should properly convert feet to meters', () => {
     //given
-    let valueOneInFeet = 5;
-    let valueTwoInFeet = 15;
+    let valueOneInFeet = 20; // 6m
+    let valueTwoInFeet = 50; // 15m
+    let valueThreeInFeet = 10; // 3
+    let valueFourInFeet = 40; // 12
+    let valueFiveInFeet = 30; // 9
+    let valueSixInFeet = 90; // 27
+    let valueSevenInFeet = 5;
+    let valueEightInFeet = 15;
 
     //when
     let valueOneInMeters = service.convertFeetToMeters(valueOneInFeet);
     let valueTwoInMeters = service.convertFeetToMeters(valueTwoInFeet);
+    let valueThreeInMeters = service.convertFeetToMeters(valueThreeInFeet);
+    let valueFourInMeters = service.convertFeetToMeters(valueFourInFeet);
+    let valueFiveInMeters = service.convertFeetToMeters(valueFiveInFeet);
+    let valueSixInMeters = service.convertFeetToMeters(valueSixInFeet);
+    let valueSevenInMeters = service.convertFeetToMeters(valueSevenInFeet);
+    let valueEightInMeters = service.convertFeetToMeters(valueEightInFeet);
 
     //then
-    expect(valueOneInMeters).toEqual(1.5);
-    expect(valueTwoInMeters).toEqual(4.5);
+    expect(valueOneInMeters).toEqual(6);
+    expect(valueTwoInMeters).toEqual(15);
+    expect(valueThreeInMeters).toEqual(3);
+    expect(valueFourInMeters).toEqual(12);
+    expect(valueFiveInMeters).toEqual(9);
+    expect(valueSixInMeters).toEqual(27);
+    expect(valueSevenInMeters).toEqual(1.5);
+    expect(valueEightInMeters).toEqual(4.5);
   });
 
   it('should properly convert miles to kilometers', () => {
