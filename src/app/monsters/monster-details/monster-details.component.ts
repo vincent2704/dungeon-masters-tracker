25a7b12bbec4a2486378b27ab9d1b0373662682a
vincent2704 <biernacki.jj@gmail.com>
2022-07-23 13:08:07 +0200

    monster info collapsing

---
+++ src/app/monsters/monster-details/monster-details.component.ts
@@ -0,0 +1,70 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Monster} from "../../models/monsters/monster";
+import {AbilityScore} from "../../models/common/ability/abilityScore";
+import {Ability} from "../../models/common/ability/ability";
+
+@Component({
+  selector: 'app-monster-details',
+  templateUrl: './monster-details.component.html',
+  styleUrls: ['./monster-details.component.css']
+})
+export class MonsterDetailsComponent implements OnInit {
+
+  @Input()
+  monster!: Monster;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getAbilityScoreInfo(abilityScore: AbilityScore): string {
+    let modifierValue = Ability.ABILITY_MODIFIERS.get(abilityScore.getScore());
+    let score = abilityScore.getScore();
+    if (modifierValue && modifierValue > 0) {
+      return `${score} (+${modifierValue})`;
+    }
+    return `${score} (${modifierValue})`;
+  }
+
+  getConditionImmunities(): string {
+    return this.monster.getConditionImmunities().map(condition => condition.getName()).join(', ');
+  }
+
+  getSenses(): string {
+    let senses = this.monster.getSenses().getMonsterSenses().map(monsterSense => {
+      let sense = monsterSense.getSense().getName();
+      let radius = monsterSense.getRadius();
+      return sense + radius;
+    }).join(', ');
+
+    let passiveSkills = this.monster.getSenses().getPassiveSkills();
+    if (passiveSkills.length > 0) {
+      let passives = passiveSkills.map(passiveSkill => {
+        let skill = passiveSkill.getSkill();
+        let score = passiveSkill.getScore();
+        return `${skill} ${score}`
+      }).join(', ');
+
+      if (senses.length > 0) {
+        return `${senses}, passive ${passives}`;
+      }
+      return `passive ${passives}`;
+    }
+    return senses;
+  }
+
+  getLanguages(): string {
+    let monsterLanguages = this.monster.getLanguages();
+    if (!monsterLanguages) {
+      return '—'
+    }
+    let languages = monsterLanguages.getLanguages().join(', ');
+    let telepathyRadius = monsterLanguages.getTelepathyRadius();
+    if (telepathyRadius) {
+      return `${languages}, telepathy ${telepathyRadius}`
+    }
+    return languages;
+  }
+
+}
--- src/app/monsters/monsters.component.ts
+++ src/app/monsters/monsters.component.ts
@@ -1,8 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {MonsterService} from "../services/monster/monster.service";
 import {Monster} from "../models/monsters/monster";
-import {AbilityScore} from "../models/common/ability/abilityScore";
-import {Ability} from "../models/common/ability/ability";
 
 @Component({
   selector: 'app-monsters',
@@ -11,61 +9,25 @@ import {Ability} from "../models/common/ability/ability";
 })
 export class MonstersComponent implements OnInit {
 
-  monsters: Monster[] = []
+  monsters: Monster[] = [];
+  monsterShowMap: Map<Monster, boolean> = new Map<Monster, boolean>();
 
   constructor(private monsterService: MonsterService) {
   }
 
   ngOnInit(): void {
     this.monsters = this.monsterService.getMonsters();
+    this.monsters.forEach(monster => {
+      this.monsterShowMap.set(monster, false);
+    })
   }
 
-  getAbilityScoreInfo(abilityScore: AbilityScore): string {
-    let modifierValue = Ability.ABILITY_MODIFIERS.get(abilityScore.getScore());
-    let score = abilityScore.getScore();
-    if (modifierValue && modifierValue > 0) {
-      return `${score} (+${modifierValue})`;
-    }
-    return `${score} (${modifierValue})`;
+  showMonster(monster: Monster): boolean {
+    return this.monsterShowMap.get(monster)!;
   }
 
-  getConditionImmunities(monster: Monster): string {
-    return monster.getConditionImmunities().map(condition => condition.getName()).join(', ');
-  }
-
-  getSenses(monster: Monster): string {
-    let senses = monster.getSenses().getMonsterSenses().map(monsterSense => {
-      let sense = monsterSense.getSense().getName();
-      let radius = monsterSense.getRadius();
-      return sense + radius;
-    }).join(', ');
-
-    let passiveSkills = monster.getSenses().getPassiveSkills();
-    if (passiveSkills.length > 0) {
-      let passives = passiveSkills.map(passiveSkill => {
-        let skill = passiveSkill.getSkill();
-        let score = passiveSkill.getScore();
-        return `${skill} ${score}`
-      }).join(', ');
-
-      if (senses.length > 0) {
-        return `${senses}, passive ${passives}`;
-      }
-      return `passive ${passives}`;
-    }
-    return senses;
-  }
-
-  getLanguages(monster: Monster): string {
-    let monsterLanguages = monster.getLanguages();
-    if (!monsterLanguages) {
-      return '—'
-    }
-    let languages = monsterLanguages.getLanguages().join(', ');
-    let telepathyRadius = monsterLanguages.getTelepathyRadius();
-    if (telepathyRadius) {
-      return `${languages}, telepathy ${telepathyRadius}`
-    }
-    return languages;
+  toggleDetails(monster: Monster) {
+    let currentShowStatus = this.monsterShowMap.get(monster);
+    this.monsterShowMap.set(monster, !currentShowStatus);
   }
 }
