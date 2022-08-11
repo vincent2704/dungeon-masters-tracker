737247236e6e8bc9755dd9e9b8b31853d54d7b1e
vincent2704 <biernacki.jj@gmail.com>
2022-08-11 10:52:18 +0200

    added action description object

---
+++ src/app/models/monsters/actions-and-traits/actionDescription.ts
@@ -0,0 +1,20 @@
+import {ActionDescriptionPoint} from "./actionDescriptionPoint";
+import {ActionType} from "./actionType";
+import {StringUtils} from "../../../utilities/string/stringUtils";
+
+export class ActionDescription {
+  constructor(
+    private readonly description: string,
+    private readonly actionType: ActionType = ActionType.NOT_SPECIFIED,
+    private readonly points: ActionDescriptionPoint[] = []
+  ) {
+  }
+
+  getDescription(): string {
+    return StringUtils.formatDescription(this.description);
+  }
+
+  getPoints(): ActionDescriptionPoint[] {
+    return this.points;
+  }
+}
---
+++ src/app/models/monsters/actions-and-traits/actionType.ts
@@ -0,0 +1,7 @@
+export enum ActionType {
+  NOT_SPECIFIED = "",
+  MELEE_WEAPON_ATTACK = "Melee Weapon Attack",
+  MELEE_OR_RANGED_WEAPON_ATTACK = "Melee or Ranged Weapon Attack",
+  MELEE_SPELL_ATTACK = "Melee Spell Attack",
+  RANGED_WEAPON_ATTACK = "Ranged Weapon Attack",
+}
