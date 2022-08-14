844cb83ba3616d36fd810ad4d391b46b2b704ed7
vincent2704 <biernacki.jj@gmail.com>
2022-08-14 10:05:25 +0200

    test for encounter service

--- src/app/services/encounter/encounter.service.spec.ts
+++ src/app/services/encounter/encounter.service.spec.ts
@@ -1,6 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 
 import { EncounterService } from './encounter.service';
+import {Encounter} from "../../models/encounter";
+import {Monster} from "../../models/monsters/monster";
+import {MonsterList} from "../../models/monsters/monsterList";
 
 describe('EncounterService', () => {
   let service: EncounterService;
@@ -13,4 +16,20 @@ describe('EncounterService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should add encounter to the list', () => {
+    // given
+    expect(service.getEncounters()).toEqual([]);
+
+    // when
+    let encounterToAdd = new Encounter('Encounter description', new Map<Monster, number>([
+      [MonsterList.WEREWOLF, 5],
+      [MonsterList.DEVA, 3],
+    ]), 'Encounter description');
+    service.addEncounter(encounterToAdd);
+
+    // then
+    expect(service.getEncounters()).toEqual([encounterToAdd])
+  });
+
 });
