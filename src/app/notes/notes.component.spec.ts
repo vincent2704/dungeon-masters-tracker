589a97b3c08be7e5099e1060c45831d889b85b2a
vincent2704 <biernacki.jj@gmail.com>
2022-12-12 20:43:08 +0100

    test fixes - note component

--- src/app/notes/notes.component.spec.ts
+++ src/app/notes/notes.component.spec.ts
@@ -4,12 +4,19 @@ import { NotesComponent } from './notes.component';
 import {NgbCollapse} from "@ng-bootstrap/ng-bootstrap";
 import {FormsModule} from "@angular/forms";
 import {NoteService} from "../services/note/note.service";
+import {Note} from "../models/note/note";
+import {Observable, of} from "rxjs";
 
 describe('NotesComponent', () => {
   let component: NotesComponent;
   let fixture: ComponentFixture<NotesComponent>;
 
   let noteServiceSpy: jasmine.SpyObj<NoteService>;
+  let defaultNote: Note = {
+    id: 1,
+    title: 'Note 1',
+    body: 'Description 1'
+  }
 
   beforeEach(async () => {
     const noteService = jasmine.createSpyObj('NoteService',
@@ -27,71 +34,92 @@ describe('NotesComponent', () => {
 
   beforeEach(() => {
     noteServiceSpy = TestBed.inject(NoteService) as jasmine.SpyObj<NoteService>;
-    // noteServiceSpy.getNotes.and.returnValue([defaultNote]);
+    noteServiceSpy.getNotes.and.returnValue(of([defaultNote]));
 
     fixture = TestBed.createComponent(NotesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  //   expect(component.notes);
-  // });
-  //
-  // it('should show note', () => {
-  //   expect(component.shownNote).toBeUndefined();
-  //
-  //   component.showNote(defaultNote);
-  //   expect(component.shownNote).toEqual(defaultNote);
-  // });
-  //
-  // it('should add note', () => {
-  //   noteServiceSpy.addNote('Title 2', 'Body 2');
-  //
-  //   expect(noteServiceSpy.addNote).toHaveBeenCalledWith('Title 2', 'Body 2');
-  //   expect(component.newNoteTitle).toEqual("");
-  //   expect(component.newNoteBody).toEqual("");
-  // });
-  //
-  // it('should delete note', () => {
-  //   component.deleteNote(defaultNote);
-  //   expect(noteServiceSpy.deleteNote).toHaveBeenCalledWith(defaultNote);
-  //   expect(component.shownNote).toBeUndefined();
-  // });
-  //
-  // it('should edit note', () => {
-  //   component.showNote(defaultNote);
-  //   component.editNote();
-  //
-  //   expect(component.editing).toBeTrue();
-  //   expect(component.editedNoteProperties.title).toEqual(defaultNote.getTitle());
-  //   expect(component.editedNoteProperties.body).toEqual(defaultNote.getBody());
-  //
-  //   let newTitle = 'New Title';
-  //   let newBody = 'New Body';
-  //
-  //   component.editedNoteProperties.title = newTitle;
-  //   component.editedNoteProperties.body = newBody;
-  //
-  //   component.submitEdit();
-  //   expect(noteServiceSpy.updateNote).toHaveBeenCalledWith(component.shownNote!, newTitle, newBody);
-  //   expect(component.editing).toBeFalse();
-  // });
-  //
-  // it('should cancel editing note', () => {
-  //   component.showNote(defaultNote);
-  //   component.editNote();
-  //
-  //   let newTitle = 'New Title';
-  //   let newBody = 'New Body';
-  //
-  //   component.editedNoteProperties.title = newTitle;
-  //   component.editedNoteProperties.body = newBody;
-  //
-  //   component.cancelEdit();
-  //   expect(noteServiceSpy.updateNote).not.toHaveBeenCalled();
-  //   expect(component.notes).toEqual([new Note('Title 1', 'Body 1')])
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+    expect(component.notesBackend);
+  });
+
+  it('should show note', () => {
+    expect(component.shownBackendNote).toBeUndefined();
+
+    component.showBackendNote(defaultNote);
+    expect(component.shownBackendNote).toEqual(defaultNote);
+  });
+
+  it('should add note', () => {
+    const noteToAdd: Note = {
+      title: 'Title',
+      body: 'Body'
+    }
+    noteServiceSpy.addNote(noteToAdd);
+
+    expect(noteServiceSpy.addNote).toHaveBeenCalledWith(noteToAdd);
+    expect(component.newNote.title).toEqual("");
+    expect(component.newNote.body).toEqual("");
+  });
+
+  it('should delete note', () => {
+    noteServiceSpy.deleteNote.withArgs(defaultNote.id!).and.returnValue(new Observable<unknown>())
+    component.deleteNote(defaultNote);
+
+    expect(noteServiceSpy.deleteNote).toHaveBeenCalledWith(defaultNote.id!);
+    expect(component.shownBackendNote).toBeUndefined();
+  });
+
+  it('should edit note', () => {
+
+    component.showBackendNote(defaultNote);
+    component.editNote();
+
+    expect(component.editing).toBeTrue();
+    expect(component.noteChanges.title).toEqual(defaultNote.title);
+    expect(component.noteChanges.body).toEqual(defaultNote.body);
+
+    let newTitle = 'New Title';
+    let newBody = 'New Body';
+
+    component.noteChanges.title = newTitle;
+    component.noteChanges.body = newBody;
+
+    noteServiceSpy.updateNote.withArgs({
+      id: 1,
+      title: newTitle,
+      body: newBody
+    }).and.returnValue(of({
+      id: 1,
+      title: newTitle,
+      body: newBody
+    }))
+
+    component.submitEdit();
+    expect(noteServiceSpy.updateNote).toHaveBeenCalledWith(component.shownBackendNote!);
+    expect(component.editing).toBeFalse();
+  });
+
+  it('should cancel editing note', () => {
+    component.showBackendNote(defaultNote);
+    component.editNote();
+
+    let newTitle = 'New Title';
+    let newBody = 'New Body';
+
+    component.noteChanges.title = newTitle;
+    component.noteChanges.body = newBody;
+
+    component.cancelEdit();
+    expect(noteServiceSpy.updateNote).not.toHaveBeenCalled();
+    expect(component.notesBackend).toEqual([{
+      id: defaultNote.id,
+      title: defaultNote.title,
+      body: defaultNote.body
+    }])
+  });
 
 });
