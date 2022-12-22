245b0c2e8cb762fc77a20e2a9d98204462e8f134
vincent2704 <biernacki.jj@gmail.com>
2022-12-23 00:00:52 +0100

    tracker - dead player bugfix

--- src/app/services/actor/actor.service.spec.ts
+++ src/app/services/actor/actor.service.spec.ts
@@ -3,6 +3,8 @@ import {TestBed} from '@angular/core/testing';
 import {ActorService} from './actor.service';
 import {Actor} from "../../models/actors/actor";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {BattleParticipantType} from "../../models/actors/battleParticipantType";
+import {PlayerCharacter} from "../../models/actors/playerCharacter";
 
 describe('actorService', () => {
   let service: ActorService;
@@ -32,5 +34,49 @@ describe('actorService', () => {
     // then
     expect(actor.isStabilized()).toBeFalse();
   });
+
+  it("should properly parse actor from JSON", () => {
+    // given
+    // const playerCharacterJson = "{ id: 1, name: \"Falimir\", level: 1, maxHp: 11, currentHp: 11, timeOfDeathEpoch: null, resurrectionPenalty: 0, availableHitDice: 1, lastLongRestTime: null }"
+    const playerCharacter: PlayerCharacter = {
+      id: 1,
+      name: 'Falimir',
+      level: 1,
+      maxHp: 11,
+      currentHp: 11,
+      timeOfDeathEpoch: undefined,
+      conditions: undefined,
+      availableHitDice: undefined,
+      resurrectionPenalty: 0
+    }
+    const expectedActor: Actor = new Actor('Falimir', 11, BattleParticipantType.PLAYER_CHARACTER, 11, 1, [], true)
+    expectedActor.id = 1
+    expectedActor.setResurrectionPenalty(0)
+    expectedActor.setTimeOfDeath(undefined);
+
+    // when
+    const result = service.fromJson(playerCharacter)
+
+    // then
+    expect(result).toEqual(expectedActor)
+  });
+
+  // fromJson(data: any): Actor {
+  //   let actor = new Actor(data.name, data.maxHp);
+  //
+  //   actor.id = data.id;
+  //   actor.currentHp = data.currentHp;
+  //   actor.level = data.level;
+  //   actor.type = BattleParticipantType.PLAYER_CHARACTER;
+  //
+  //   const timeOfDeath = data.timeOfDeathEpoch
+  //     ? new Date(data.timeOfDeathEpoch)
+  //     : undefined
+  //   console.log(`${data.name} timeOfDeath: ${timeOfDeath}`)
+  //   actor.setTimeOfDeath(timeOfDeath);
+  //   actor.setResurrectionPenalty(data.resurrectionPenalty);
+  //
+  //   return actor;
+  // }
 
 });
