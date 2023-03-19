9c176c335140933116949682e6ed0b09437e7815
vincent2704 <biernacki.jj@gmail.com>
2023-03-19 18:30:58 +0100

    test compilation fix

--- src/app/welcome-screen/welcome-screen.component.spec.ts
+++ src/app/welcome-screen/welcome-screen.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WelcomeScreenComponent } from './welcome-screen.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('WelcomeScreenComponent', () => {
   let component: WelcomeScreenComponent;
@@ -8,6 +10,7 @@ describe('WelcomeScreenComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ FormsModule, HttpClientTestingModule ],
       declarations: [ WelcomeScreenComponent ]
     })
     .compileComponents();
