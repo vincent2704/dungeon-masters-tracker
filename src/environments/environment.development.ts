215643a2537fb87727123c49f31050440f0584c0
vincent2704 <biernacki.jj@gmail.com>
2023-04-13 17:12:09 +0200

    added temporary load balancer apiUrl for dev environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,5 +3,5 @@ import {Environment} from "../app/environment";
 export const environment = {
   production: true,
   environmentName: Environment.DEV,
-  apiUrl: 'http://3.72.67.102:8080'
+  apiUrl: 'http://dm-tracker-dev-backend-202130064.eu-central-1.elb.amazonaws.com'
 };
