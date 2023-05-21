4b730b5626543ede7dcdc916da6159e8f2e8be8a
vincent2704 <biernacki.jj@gmail.com>
2023-05-21 16:17:13 +0200

    test refactor - beforeEach

--- cypress/e2e/campaignManagement.cy.ts
+++ cypress/e2e/campaignManagement.cy.ts
@@ -5,10 +5,7 @@ describe('campaign adding and deleting', () => {
   const campaignName = 'Cypress test campaign'
 
   beforeEach(() => {
-    cy.visit('http://localhost:4200')
-  });
-
-  it('creates new campaign', () => {
+    cy.visit('http://localhost:4200');
     cy.get('#login-tab').should('be.visible')
       .click();
 
@@ -17,7 +14,9 @@ describe('campaign adding and deleting', () => {
 
     cy.get('#login-password-input').type(testPassword);
     cy.get('#login-submit-button').click();
+  });
 
+  it('creates new campaign', () => {
     cy.get('#campaign-selector').should('be.visible');
     cy.get('#create-campaign-button').should('be.visible').click();
     cy.get('#create-campaign-name-input').should('be.visible')
@@ -34,7 +33,6 @@ describe('campaign adding and deleting', () => {
     cy.get('#create-campaign-submit-button').should('be.disabled');
     cy.get('#create-campaign-start-time-picker').should('be.visible')
       .within(() => {
-        // znalezc liste inputow i wybrac pierwsyz, drugi i trzeci bo po id to ciezko
         cy.get('input').first().type('14');
         cy.get('input').eq(1).type('30');
         cy.get('input').eq(2).type('0');
@@ -53,11 +51,6 @@ describe('campaign adding and deleting', () => {
   })
 
   it('fails to create another campaign with existing name', () => {
-    cy.get('#login-username-input').type(testUsername).click();
-
-    cy.get('#login-password-input').type(testPassword);
-    cy.get('#login-submit-button').click();
-
     cy.get('#create-campaign-button').click();
     cy.get('#create-campaign-name-input')
       .type(campaignName);
@@ -81,18 +74,15 @@ describe('campaign adding and deleting', () => {
 
     cy.get('#campaign-creation-failed-modal').should('be.visible');
     cy.get('#bad-credentials-modal-close-button').click();
-  })
+  });
+
+  it('property displays campaign information', () => {
+    cy.contains(campaignName).click();
+    cy.get('#campaign-overview-campaign-name').should('have.text', campaignName)
+
+  });
 
   it('deletes campaign', () => {
-    cy.get('#login-tab').should('be.visible')
-      .click();
-
-    cy.get('#login-username-input').should('be.visible')
-      .type(testUsername).click();
-
-    cy.get('#login-password-input').type(testPassword);
-    cy.get('#login-submit-button').click();
-
     cy.contains(campaignName);
     cy.contains('Delete').click();
     cy.get('#campaign-deleted-modal').should('be.visible');
